07539efe3d936416953e5569a13aead9a8660011
oceanroleplay <indianoceanroleplay@gmail.com>
2022-02-13 22:17:15 +0530

    chore: dep update

--- src/commands/slash group.ts
+++ src/commands/slash group.ts
@@ -1,38 +1,80 @@
-import { CommandInteraction } from "discord.js";
-import { Discord, Slash, SlashOption, SlashGroup } from "discordx";
+import type { CommandInteraction, GuildMember, Role, User } from "discord.js";
+
+import { Discord, Slash, SlashChoice, SlashGroup, SlashOption } from "discordx";
 
 enum TextChoices {
-  Hello = "Hello",
   "Good Bye" = "GoodBye",
+  Hello = "Hello",
 }
 
 @Discord()
-@SlashGroup("testing", "Testing group description", {
-  maths: "maths group description",
-  text: "text group description",
-})
-export abstract class AppDiscord {
-  @Slash("add")
-  @SlashGroup("maths")
-  add(
-    @SlashOption("x", { description: "x value" })
-    x: number,
-    @SlashOption("y", { description: "y value" })
-    y: number,
+@SlashGroup({ name: "testing" })
+@SlashGroup({ name: "maths", root: "testing" })
+@SlashGroup({ name: "text", root: "testing" })
+export abstract class Group {
+  @Slash("voicechannel")
+  @SlashGroup({ name: "maths", root: "testing" })
+  voicechannel(
+    @SlashOption("channel", {
+      channelTypes: ["GUILD_CATEGORY", "GUILD_VOICE", "GUILD_TEXT"],
+      type: "CHANNEL",
+    })
+    roleOrUser: GuildMember | User | Role,
     interaction: CommandInteraction
-  ) {
+  ): void {
+    interaction.reply(`${roleOrUser}`);
+  }
+
+  @Slash("voicechannelx")
+  voicechannelx(
+    @SlashOption("channel", {
+      channelTypes: ["GUILD_CATEGORY", "GUILD_VOICE", "GUILD_TEXT"],
+      type: "CHANNEL",
+    })
+    roleOrUser: GuildMember | User | Role,
+    interaction: CommandInteraction
+  ): void {
+    interaction.reply(`${roleOrUser}`);
+  }
+
+  @Slash("add")
+  @SlashGroup({ name: "maths", root: "testing" })
+  add(
+    @SlashOption("x", { description: "x value" }) x: number,
+    @SlashOption("y", { description: "y value" }) y: number,
+    interaction: CommandInteraction
+  ): void {
     interaction.reply(String(x + y));
   }
 
   @Slash("multiply")
-  @SlashGroup("maths")
+  @SlashGroup({ name: "maths", root: "testing" })
   multiply(
-    @SlashOption("x", { description: "x value" })
-    x: number,
-    @SlashOption("y", { description: "y value" })
-    y: number,
+    @SlashOption("x", { description: "x value" }) x: number,
+    @SlashOption("y", { description: "y value" }) y: number,
     interaction: CommandInteraction
-  ) {
+  ): void {
     interaction.reply(String(x * y));
+  }
+
+  @Slash("hello")
+  @SlashGroup({ name: "text", root: "testing" })
+  hello(
+    @SlashChoice(TextChoices)
+    @SlashOption("text", {
+      type: "STRING",
+    })
+    text: TextChoices,
+    interaction: CommandInteraction
+  ): void {
+    interaction.reply(text);
+  }
+
+  @Slash("hello")
+  root(
+    @SlashOption("text") text: string,
+    interaction: CommandInteraction
+  ): void {
+    interaction.reply(text);
   }
 }
