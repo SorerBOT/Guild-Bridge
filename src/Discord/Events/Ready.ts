bfb474be4144f5854a1f495ccba20b71de54fd55
SorerBOT <alonfiller@gmail.com>
2022-07-07 18:32:20 +0300

    Added message Hello Keren!

--- src/Discord/Events/Ready.ts
+++ src/Discord/Events/Ready.ts
@@ -2,5 +2,6 @@ import Discord from "discord.js";
 import LogEvent from "../../Log/Events/LogEvent";
 
 export default function Ready(client: Discord.Client) {
+    console.log("Hello Keren!");
     LogEvent("Ready", (client.user?.username ?? "Guild-Bridge") + " has logged on!", true);
 }
