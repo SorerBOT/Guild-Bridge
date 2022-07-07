e3c4597a5f3d27b9cc13aeb3c66e8698c472dfd2
SorerBOT <alonfiller@gmail.com>
2022-07-07 18:39:06 +0300

    Removed message Hello Keren (Demonstration to mother)

--- src/Discord/Events/Ready.ts
+++ src/Discord/Events/Ready.ts
@@ -2,6 +2,5 @@ import Discord from "discord.js";
 import LogEvent from "../../Log/Events/LogEvent";
 
 export default function Ready(client: Discord.Client) {
-    console.log("Hello Keren!");
     LogEvent("Ready", (client.user?.username ?? "Guild-Bridge") + " has logged on!", true);
 }
